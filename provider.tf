830bd607022b64d4b1e0cb41bad4a345d26dd575
dgnnj <deciogoncalvesjr@hotmail.com>
2025-02-04 18:30:37 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.85.0"
     }
+  }
+  backend "s3" {
+    bucket = "projetodevops-s3"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
