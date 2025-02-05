882a4190256e6cc0549e6a4d576f829c40b8a6b9
dgnnj <deciogoncalvesjr@hotmail.com>
2025-02-05 18:17:22 -0300

    feat: private subnets and variables

--- public.tf
+++ public.tf
@@ -1,11 +1,13 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone = "${data.aws_region.current.name}a"
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current.name}a"
+  map_public_ip_on_launch = true
+
   tags = merge(
     local.tags,
     {
-      Name                     = "projectdevops-subnet-1a",
+      Name                     = "${var.project_name}-pub-subnet-1a",
       "kubernetes.io/role/elb" = 1
     }
   )
@@ -16,10 +18,11 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
+
   tags = merge(
     local.tags,
     {
-      Name                     = "projectdevops-subnet-1b",
+      Name                     = "${var.project_name}s-pub-subnet-1b",
       "kubernetes.io/role/elb" = 1
     }
   )
