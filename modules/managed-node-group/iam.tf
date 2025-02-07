9891704d81eb8ebb4e0903b8e530a5dba2f49536
dgnnj <deciogoncalvesjr@hotmail.com>
2025-02-07 18:43:59 -0300

    fix: iam role and new feat node instance template

--- modules/managed-node-group/iam.tf
+++ modules/managed-node-group/iam.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "eks_mng_role" {
-  name = "${var.project_name}-mng-role"
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.project_name}-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -18,22 +18,32 @@ resource "aws_iam_role" "eks_mng_role" {
   tags = merge(
     var.tags,
     {
-      Name = "${var.project_name}-mng-role"
+      Name = "${var.project_name}-node-role"
     }
   )
 }
 
-resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment_worker" {
-  role       = aws_iam_role.eks_mng_role.name
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment_ecr" {
-  role       = aws_iam_role.eks_mng_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment_cni" {
-  role       = aws_iam_role.eks_mng_role.name
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEC2ContainerRegistryPullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonVPCFullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEC2FullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role       = aws_iam_role.eks_node_role.name
 }
--- modules/managed-node-group/mng.tf
+++ modules/managed-node-group/mng.tf
@@ -1,11 +1,36 @@
+resource "aws_launch_template" "eks_launch_template" {
+  name_prefix = "${var.project_name}-launch-template"
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "optional"
+  }
+
+  instance_type = "t3.small"
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      var.tags,
+      {
+        Name = "${var.project_name}-node"
+      }
+    )
+  }
+}
+
 resource "aws_eks_node_group" "eks_managed_node_group" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.project_name}-nodegroup"
-  node_role_arn   = aws_iam_role.eks_mng_role.arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
   subnet_ids = [
     var.subnet_private_1a,
     var.subnet_private_1b
   ]
+
+  capacity_type = "SPOT"
 
   scaling_config {
     desired_size = 1
@@ -13,10 +38,15 @@ resource "aws_eks_node_group" "eks_managed_node_group" {
     min_size     = 1
   }
 
+  launch_template {
+    id      = aws_launch_template.eks_launch_template.id
+    version = aws_launch_template.eks_launch_template.latest_version
+  }
+
   depends_on = [
-    aws_iam_role_policy_attachment.eks_mng_role_attachment_worker,
-    aws_iam_role_policy_attachment.eks_mng_role_attachment_ecr,
-    aws_iam_role_policy_attachment.eks_mng_role_attachment_cni
+    aws_iam_role_policy_attachment.eks_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks_AmazonEC2ContainerRegistryPullOnly,
+    aws_iam_role_policy_attachment.eks_AmazonEKS_CNI_Policy
   ]
 
   tags = merge(
